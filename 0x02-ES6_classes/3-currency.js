5b71dbfce31ed72446c3ff7025cf04621af1ca9d
Shalom Ogoziem <shalomzymike@gmail.com>
2024-01-04 14:29:56 +0000

    es6 classes

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,41 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  /* getter and setter for code */
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  /* getter and setter for name */
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  /* display the full currency */
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
