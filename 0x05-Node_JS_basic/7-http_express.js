87ffdd998c94c124fe818e251887b47723373229
shalom ogoziem <shalomzymike@gmail.com>
2024-02-27 23:36:44 +0100

    7-express_http

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,65 +1,58 @@
 const express = require('express');
-const fs = require('fs').promises;
+const fs = require('fs');
 
 const app = express();
 
-async function readDatabase(path) {
+async function studentsData(path) {
+  let data;
   try {
-    const data = await fs.readFile(path, 'utf8');
-    const lines = data.trim().split('\n');
-
-    if (lines.length <= 1) {
-      return 'Number of students: 0';
-    }
-
-    const students = {};
-
-    lines.slice(1).forEach((line) => {
-      const fields = line.split(',');
-      const field = fields[fields.length - 1].trim();
-
-      if (!students[field]) {
-        students[field] = [];
-      }
-
-      students[field].push(fields[0]);
-    });
-
-    let result = '';
-    let totalStudents = 0;
-
-    const fieldKeys = Object.keys(students).sort();
-    fieldKeys.forEach((field) => {
-      const numStudents = students[field].length;
-      totalStudents += numStudents;
-      result += `Number of students in ${field}: ${numStudents}. List: ${students[field].join(', ')}\n`;
-    });
-
-    if (totalStudents > 0) {
-      result = `Number of students: ${totalStudents}\n${result}`;
-    }
-
-    return result;
+    data = await fs.promises.readFile(path, 'utf8');
   } catch (error) {
     throw new Error('Cannot load the database');
   }
+  const students = data.split('\n')
+    .map((student) => student.split(','))
+    .filter((student) => student.length === 4 && student[0] !== 'firstname')
+    .map((student) => ({
+      firstName: student[0],
+      lastName: student[1],
+      age: student[2],
+      field: student[3],
+    }));
+  const csStudents = students
+    .filter((student) => student.field === 'CS')
+    .map((student) => student.firstName);
+  const sweStudents = students
+    .filter((student) => student.field === 'SWE')
+    .map((student) => student.firstName);
+  console.log(`Number of students: ${students.length}`);
+  console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+  console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+  return { students, csStudents, sweStudents };
 }
 
 app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
-  try {
-    const data = await readDatabase(process.argv[2]);
-    res.send(data);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('This is the list of our students\n');
+  await studentsData(process.argv[2]).then((data) => {
+    res.write(`Number of students: ${data.students.length}\n`);
+    res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
+  }).catch((err) => res.write(err.message))
+    .finally(() => {
+      res.end();
+    });
 });
 
 app.listen(1245, () => {
-  console.log('Server is running at http://localhost:1245/');
+  console.log('Server running at http://localhost:1245');
 });
 
 module.exports = app;
