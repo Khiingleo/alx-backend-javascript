395f6d69fda096b734d6b30d7abec79803184080
shalom ogoziem <shalomzymike@gmail.com>
2024-02-27 23:10:27 +0100

    testing again

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,72 +1,72 @@
 const http = require('http');
 const fs = require('fs');
 
-const countStudents = (path) => new Promise((resolve, reject) => {
-  fs.readFile(path, 'utf-8', (err, data) => {
-    if (err) {
-      reject(new Error('Cannot load the database'));
-    } else {
+function getStudentsData(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
       const lines = data.trim().split('\n');
-      const counters = {};
 
-      lines.forEach((line, idx) => {
-        if (idx === 0) {
-          return;
+      if (lines.length <= 1) {
+        resolve({ totalStudents: 0, details: {} });
+        return;
+      }
+
+      const students = {};
+
+      lines.slice(1).forEach((line) => {
+        const fields = line.split(',');
+        const field = fields[fields.length - 1].trim();
+
+        if (!students[field]) {
+          students[field] = [];
         }
-        // eslint-disable-next-line no-unused-vars
-        const [firstName, lastName, age, field] = line.split(',');
-        if (field) {
-          if (!counters[field]) {
-            counters[field] = { count: 0, names: [] };
-          }
-          // eslint-disable-next-line no-plusplus
-          counters[field].count++;
-          counters[field].names.push(firstName);
-        }
+
+        students[field].push(fields[0]);
       });
 
-      const studentsData = Object.entries(counters).map(([field, data]) => {
-        const numbers = data.count;
-        const names = data.names.join(', ');
-        return `Number of students in ${field}: ${numbers}. List: ${names}`;
-      }).join('\n');
+      let totalStudents = 0;
 
-      resolve(studentsData);
-    }
+      Object.keys(students).forEach((field) => {
+        totalStudents += students[field].length;
+      });
+
+      resolve({ totalStudents, details: students });
+    });
   });
-});
+}
 
 const app = http.createServer((req, res) => {
-  const handleError = (error) => {
-    res.writeHead(500, { 'Content-Type': 'text/plain' });
-    res.end(error.message);
-  };
-
-  const sendResponse = (statusCode, data) => {
-    res.writeHead(statusCode, { 'Content-Type': 'text/plain' });
-    res.end(data);
-  };
-
   if (req.url === '/') {
-    sendResponse(200, 'Hello Holberton School!');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!\n');
   } else if (req.url === '/students') {
-    countStudents(process.argv[2])
-      .then((studentData) => {
+    getStudentsData(process.argv[2])
+      .then(({ totalStudents, details }) => {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.write('This is the list of our students\n');
-        res.write(studentData);
+        res.write(`Number of students: ${totalStudents}\n`);
+        Object.keys(details).forEach((field) => {
+          const numStudents = details[field].length;
+          res.write(`Number of students in ${field}: ${numStudents}. List: ${details[field].join(', ')}\n`);
+        });
         res.end();
       })
       .catch((error) => {
-        handleError(error);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(`${error.message}\n`);
       });
   } else {
-    sendResponse(404, 'Not Found\n');
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not found\n');
   }
 });
 
 app.listen(1245, () => {
-  console.log('Server is running at http://localhost:1245');
+  console.log('Server running at http://localhost:1245/');
 });
 
 module.exports = app;
