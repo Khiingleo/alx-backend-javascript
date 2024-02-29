1180c4ab1fbb01b3e4a796bd1e79eecf49286b78
shalom ogoziem <shalomzymike@gmail.com>
2024-02-29 21:31:30 +0100

    async tests with done()

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,9 @@
+const getPaymentTokenFromAPI = (success) => {
+    return new Promise((res, rej) => {
+        if (success) {
+            res({ data: 'Successful response from the API'});
+        }
+    });
+};
+
+module.exports = getPaymentTokenFromAPI;
