12bd2e6dc3821696bc428831ebd4adcc28e3b955
shalom ogoziem <shalomzymike@gmail.com>
2024-02-29 22:42:11 +0100

    deep equality and post integration testing

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to the payment system');
