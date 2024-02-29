8fccd5e7fc2b7b33267919936165065ca1e44584
shalom ogoziem <shalomzymike@gmail.com>
2024-02-29 22:01:54 +0100

    regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+    it('Correct status code and response?', (done) => {
+        request('http://localhost:7865/', (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.be.equal('Welcome to the payment system');
+            done();
+        });
+    });
+    it('Response message for valid :id', (done) => {
+        request('http://localhost:7865', (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('Payment methods for cart 12');
+            done();
+        });
+    });
+    it ('Correct status code when :id is not a bumber', (done) => {
+        request('http://localhost:7865', (error, response) => {
+            expect(response.statusCode).to.equal(404);
+            done();
+        });
+    });
+});
